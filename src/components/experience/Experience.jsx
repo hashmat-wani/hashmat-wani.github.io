9a7b65282e05052b532c4eb03dbb8c2b75d8517a
Hashmat-Noorani <hashmatwani@icloud.com>
2022-12-10 17:00:47 +0530

    modified: experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,28 +1,29 @@
-import React from "react";
+import { setElementStyles } from "parallax-controller";
+import React, { useLayoutEffect, useState } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
 import styled from "styled-components";
 import PageHeader from "../common/PageHeader";
 import { ExperienceWrapper, ExperienceDetails } from "./Experience.style";
 
 const Experience = () => {
-  let date = new Date();
-  console.log(date.getMonth());
-  console.log(date.getFullYear());
-
   const companies = [
     {
-      id: 1,
+      id: "c1",
       name: "Masai School",
       location: "Remote",
       desc: "Masai is an outcome-driven career school whose mission is to skill India’s untapped & underutilized talent and train them for some of the most in-demand jobs in the world.",
+      logo: "https://media-exp1.licdn.com/dms/image/C560BAQHdxMHntX6Z8Q/company-logo_200_200/0/1650372038245?e=1678924800&v=beta&t=JJF4S3MPdH5DviPExenbpu9gfgakuhPYmITKKsOITx4",
     },
   ];
   const experiences = [
     {
+      id: "e1",
       title: "Instructional Associate",
       employment_type: "Full-time",
-      company_id: 1,
+      company_id: "c1",
       currently_working: true,
-      start_date: "Apr-2022",
+      start_date: [4, 2022],
       end_date: false,
       desc: "Roles & Responsibilities",
       bulletPoints: [
@@ -47,41 +48,141 @@ const Experience = () => {
       ],
     },
     {
+      id: "e2",
       title: "Student Guide",
       employment_type: "Part-time",
-      company_id: 1,
+      company_id: "c1",
       currently_working: false,
-      start_date: "Feb-2022",
-      end_date: "Mar-2022",
+      start_date: [2, 2022],
+      end_date: [3, 2022],
+      // end_date: false,
       desc: "Student Guide in Masai works with 10 students on average and thus they have the bandwidth to connect with students for their smallest of queries.",
       bulletPoints: false,
       skills: false,
     },
   ];
+  const monthNames = [
+    "",
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  let date = new Date();
+  const currMonth = date.getMonth() + 1;
+  const currYear = date.getFullYear();
+  experiences.forEach((exp) => {
+    if (
+      exp.start_date[0] === exp.end_date[0] &&
+      exp.start_date[1] === exp.end_date[1]
+    ) {
+      exp.duration = `${monthNames[exp.start_date[0]]} ${
+        exp.start_date[1]
+      } · 1 mo`;
+    } else {
+      let total_mos = 0;
+      if (exp.end_date) {
+        total_mos =
+          (exp.end_date[1] - exp.start_date[1]) * 12 +
+          (exp.end_date[0] - exp.start_date[0] + 1);
+      } else {
+        total_mos =
+          (currYear - exp.start_date[1]) * 12 +
+          (currMonth - exp.start_date[0] + 1);
+      }
+
+      const company = companies.find((comp) => comp.id === exp.company_id);
+      company.duration = company.duration + total_mos || total_mos;
+
+      const yrs = Math.floor(total_mos / 12);
+      const mos = total_mos % 12;
+
+      exp.duration = `${monthNames[exp.start_date[0]]} ${exp.start_date[1]} - ${
+        exp.end_date
+          ? `${monthNames[exp.end_date[0]]} ${exp.end_date[1]} `
+          : "Present"
+      } · ${yrs === 1 ? `${yrs} yr` : yrs > 1 ? `${yrs} yrs` : ""}
+      ${mos === 1 ? `${mos} mo` : mos > 1 ? `${mos} mos` : ""}`;
+    }
+  });
+
+  companies.forEach((comp) => {
+    if (comp.duration) {
+      const yrs = Math.floor(comp.duration / 12);
+      const mos = comp.duration % 12;
+      comp.duration = `${yrs === 1 ? `${yrs} yr` : yrs > 1 ? `${yrs} yrs` : ""}
+        ${mos === 1 ? `${mos} mo` : mos > 1 ? `${mos} mos` : ""}`;
+    }
+  });
+  const [expLeftDivSize, setexpLeftDivSize] = useState([]);
+  const ref = useRef(null);
+  useLayoutEffect(() => {
+    // setexpLeftDivSize([...ref.current.children]);
+    const handleResize = () => setexpLeftDivSize([...ref.current.children]);
+    // console.log(ref.current.childNodes[0]);
+    // console.log(ref.current.children[0].clientHeight);
+    // console.log(ref.current.childNodes[0].clientHeight);
+    // console.log(ref.current.children);
+    // ref.current.children[0].style.height = "400px";
+    // [...ref.current.children].map((el, idx) => {
+    //   console.log(el);
+    // });
+
+    window.addEventListener("resize", handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  // window.addEventListener("resize", () => setexpLeftDivSize2(!test2));
+  // console.log(test[0].clientHeight);
+  // ref.current.childNodes.length;
+
   return (
     <ExperienceWrapper>
       <PageHeader>Experience</PageHeader>
+
       <ExperienceDetails>
         {companies.map((comp) => (
           <CompanyDetails key={comp.id}>
             <Left>
-              <Logo>
-                <img
-                  src="https://media-exp1.licdn.com/dms/image/C560BAQHdxMHntX6Z8Q/company-logo_200_200/0/1650372038245?e=1678924800&v=beta&t=JJF4S3MPdH5DviPExenbpu9gfgakuhPYmITKKsOITx4"
-                  alt="logo"
-                  width="100%"
-                />
-              </Logo>
+              {ref.current &&
+                expLeftDivSize.map((el, idx) => (
+                  <div key={idx} style={{ height: el.offsetHeight }}>
+                    {idx === 0 ? (
+                      <Logo>
+                        <img src={comp.logo} alt="logo" width="100%" />
+                      </Logo>
+                    ) : (
+                      <>
+                        <div className="side_bullet"></div>
+                        {idx < expLeftDivSize.length - 1 && (
+                          <div
+                            className="side_vertical_line"
+                            style={{ height: el.offsetHeight - 12 }}
+                          ></div>
+                        )}
+                      </>
+                    )}
+                  </div>
+                ))}
             </Left>
-            <Right>
+
+            <Right ref={ref}>
               <Company>
                 <Heading>
                   <h3>{comp.name}</h3>
+                  {comp.duration && <p>{comp.duration}</p>}
                   <p>{comp.location}</p>
-                  {/* <p>
-                    {comp.start_date} -{" "}
-                    {comp.end_date ? comp.end_date : "Present"}
-                  </p> */}
                 </Heading>
                 <Details>
                   <p>{comp.desc}</p>
@@ -90,20 +191,19 @@ const Experience = () => {
               {experiences
                 .filter((expp) => expp.company_id === comp.id)
                 .map((exp) => (
-                  <Experiences>
+                  <Experiences key={exp.id}>
                     <Heading>
                       <h4>{exp.title}</h4>
                       <p>{exp.employment_type}</p>
-                      <p>
-                        {exp.start_date} -{" "}
-                        {exp.end_date ? exp.end_date : "Present"}
-                      </p>
+                      <p>{exp.duration}</p>
                     </Heading>
                     <Details>
                       <p>{exp.desc}</p>
                       <ul>
                         {exp.bulletPoints &&
-                          exp.bulletPoints.map((pt) => <li>{pt}</li>)}
+                          exp.bulletPoints.map((pt, idx) => (
+                            <li key={idx}>{pt}</li>
+                          ))}
                       </ul>
                       {exp.skills && (
                         <p>
@@ -152,19 +252,44 @@ const CompanyDetails = styled.div`
 const Left = styled.div`
   max-width: 50px;
   /* border: 1px solid blue; */
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  /* padding-top: 3px; */
+  > div {
+    /* border: 1px solid red; */
+  }
+  .side_bullet {
+    background: ${(props) =>
+      props.theme.dark ? props.theme.primaryText : "#0000004D"};
+    height: 8px;
+    width: 8px;
+    border-radius: 50%;
+    margin: 8px auto;
+  }
+  .side_vertical_line {
+    background: ${(props) =>
+      props.theme.dark ? props.theme.primaryText : "#00000014"};
+    width: 2px;
+    margin: 0 auto;
+    border-radius: 10px;
+  }
 `;
 const Right = styled.div`
   /* border: 1px solid green; */
   display: flex;
   flex-direction: column;
   gap: 12px;
+  > div {
+    /* border: 1px solid green; */
+  }
 `;
 const Company = styled.div``;
 
 const Experiences = styled.div``;
 
 const Heading = styled.div`
-  margin-bottom: 10px;
+  margin-bottom: 9px;
 `;
 const Details = styled.div`
   /* border: 1px solid red; */
